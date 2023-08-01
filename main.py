753894cc19a43e63451506466b6160c86732a287
banderastoni <banderastoni@rambler.ru>
2023-08-01 12:23:23 +0300

    1. upgrade main.py (interaction with choice of platform)

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
+import json
+
+from Classes.apiclasses import HeadHunterAPI, SuperJobAPI
+from Classes.Vacancy import Vacancy
+
 # Создание экземпляра класса для работы с API сайтов с вакансиями
 hh_api = HeadHunterAPI()
-### superjob_api = SuperJobAPI()
+superjob_api = SuperJobAPI()
 
 # Получение вакансий с разных платформ
 hh_vacancies = hh_api.get_vacancies("Python")
-### superjob_vacancies = superjob_api.get_vacancies("Python")
-
-# Создание экземпляра класса для работы с вакансиями
-vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.",
-                  "Требования: опыт работы от 3 лет...")
-
-# Сохранение информации о вакансиях в файл
-json_saver = JSONSaver()
-json_saver.add_vacancy(vacancy)
-json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-json_saver.delete_vacancy(vacancy)
+superjob_vacancies = superjob_api.get_vacancies("Python")
 
 
-# Функция для взаимодействия с пользователем
 def user_interaction():
-    platforms = ["HeadHunter", "SuperJob"]
-    search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
+    platforms = ['1', '2']
+    platform = 0
+    all_vacancies = []
+    # while platform not in platforms:
+    #     platform = input("Выберите платформу (1 или 2):\n   1) HeadHunter\n   2) SuperJob\n")
+    #     if platform not in platforms:
+    #         print("Неправильный ввод. Попробуйте ещё раз.\n")
+    # while platform not in platforms:
+    #     platform = [input("Выберите платформу: HeadHunter или SuperJob.\n") while platform != platforms]
+    #     print("Неправильный ввод. Попробуйте ещё раз.\n") if platform not in platforms else "Корректно"
 
-    if not filtered_vacancies:
-        print("Нет вакансий, соответствующих заданным критериям.")
-        return
+    # platform = [input("Выберите платформу (1 или 2):\n   1) HeadHunter\n   2) SuperJob\n") for platform in platforms]
+    while platform not in platforms:
+        platform = input("Выберите платформу (1 или 2):\n   1) HeadHunter\n   2) SuperJob\n")
+        print("Вы выбрали HeadHunter") if platform == '1' else 'Вы выбрали SuperJob'
+        match platform:
+            case '1':
+                for vacancy in hh_vacancies:
+                    hh_vacancy = Vacancy(vacancy['name'], vacancy['salary']['from'], vacancy['salary']['to'],
+                                         vacancy['alternate_url'],
+                                         vacancy['snippet'])
+                    all_vacancies.append(hh_vacancy)
+            case '2':
+                for vacancy in superjob_vacancies:
+                    sj_vacancy = Vacancy(vacancy['profession'], vacancy['payment_from'], vacancy['payment_to'],
+                                         vacancy['link'],
+                                         vacancy['candidat'])
+                    all_vacancies.append(sj_vacancy)
+            case _:
+                print("Неправильный ввод. Попробуйте ещё раз.\n")
 
-    sorted_vacancies = sort_vacancies(filtered_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
+    vacs = []
+
+    for vac in all_vacancies:
+        vacs.append(vac.__dict__)
+
+    with open('json/all_vacancies.json', 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(vacs, indent=2, ensure_ascii=False))
 
 
 if __name__ == "__main__":
